0194c908bd040bc49f9e05dc55dae168af93de0e
William Lawrence <will.lawrence.3rd@gmail.com>
2020-06-26 05:41:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,21 @@
-
+def sort_array_asc(arr)
+  arr.sort
+end
+def sort_array_desc(arr)
+  arr.sort do |x,y|
+    y<=>x
+  end
+end
+def swap_elements(arr)
+  arr[2], arr[1] = arr[1], arr[2]
+  arr
+end
+def reverse_array(arr)
+  arr.reverse
+end
+def kesha_maker(arr)
+  for i in 0...arr.length
+    arr[i][2] = "$"
+  end
+  arr
+end
